12f846f9933bd9310f0c98bc70c34df3c8eda4d1
Mike Nimer <mikenimer@yahoo.com>
2014-11-22 16:22:42 -0600

    tweak the core filter

--- src/main/java/com/familydam/core/filters/SimpleCorsFilter.java
+++ src/main/java/com/familydam/core/filters/SimpleCorsFilter.java
@@ -40,9 +40,9 @@ public class SimpleCorsFilter implements Filter
     {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "origin, x-requested-with, x-csrf-token, content-type, accept, authentication, authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         chain.doFilter(req, res);
     }
 
